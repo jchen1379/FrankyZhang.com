6db3af9b24d7b8856a2d17e85f6395e8605b0946
jchen1379 <jianhuichen1379@gmail.com>
2021-09-08 13:17:51 -0400

    update time for waking up herokuapp

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const WakeUpHeroku = () => {
   request.send();
 }
 
-setInterval(WakeUpHeroku, 25*60*1000);
+setInterval(WakeUpHeroku, 10000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
